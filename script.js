80f0b21c9a51cf3513450796c9544946f8043dae
Sandy Raveloarisoa <sandy.rav@onja.org>
2020-07-13 07:52:19 +0300

    I created the delete button and put the new post at the begining of the card

--- script.js
+++ script.js
@@ -1,55 +1,34 @@
 console.log('it works');
-const myPost = () => {
-const divCard = document.createElement('div');
-divCard.classList.add('card');
+  const newPostList = document.getElementById('post-list');
+  const divCard = document.querySelector('.card');
+  const cardBody = document.getElementsByClassName('card-body');
+  const cardTitle = document.getElementsByClassName('post-title');
+  const cardText = document.getElementsByClassName('card-text');
+  const cardAuthor = document.querySelector(`[name = "postAuthor"]`);
+  const deleteButton = document.getElementById('delete-button');
+  const submitButton = document.getElementById('submit-button');
 
-const myImg = document.createElement('img');
-const sourceImg = document.getElementById('post-url').value;
-myImg.setAttribute('src', `${sourceImg}`);
-myImg.classList.add('card-img-top');
-myImg.alt = 'Card image cap';
-myImg.textContent = `${sourceImg.value}`;
-divCard.appendChild(myImg);
-
-const cardBody = document.createElement('div');
-cardBody.classList.add('card-body');
-divCard.appendChild(cardBody);
-
-const cardTitle = document.createElement('h5');
-cardTitle.classList.add('card-title');
-cardTitle.textContent = `${cardTitle.value}`;
-cardBody.appendChild(cardTitle);
-
-const cardText = document.createElement('p');
-cardText.classList.add('card-text');
-cardText.textContent = `${cardText.value}`;
-cardBody.appendChild(cardText);
-return divCard;
-} 
-
-const submitButton = document.getElementById('submit-button');
-const newPostList = document.getElementById('post-list');
-submitButton.addEventListener('click', ($event) => {
-  const realPost = myPost();
-  newPostList.appendChild(realPost);//Here we append the new post inside of the div
-  $event.preventDefault();
-  // And we will try to reset the form so that we can write new inputs
-  document.querySelector('form-group').reset();
-});
-
-const deleteButton = document.createElement('button');
-deleteButton.type = `button`;
-deleteButton.classList.add('btn', 'btn-sm', 'btn-light', 'btn-delete');
-deleteButton.textContent = 'delte entry';
-const parent = document.getElementsByClassName('card-body');
-parent.appendChild(deleteButton);
-
-// make out delete function ($event)
-const deleteFunction = ($event) => {
-	$event.currentTarget.parentElement.remove();
-};
-// way to delete the card : $event.currentTarget.parentElement.remove();
-// loop over them and attach a listener
-deleteButton.forEach((button) => {
-	button.addEventListener('click', deleteFunction);
+  const myPost = () => {
+    const myHtml =`
+    <div class="card">
+					<img
+						class="card-img-top"
+						src="https://picsum.photos/500/200"
+						alt="Card image cap"
+					/>
+					<div class="card-body">
+						<h5 class="card-title">${cardTitle.value} <small> by ${cardAuthor.value}</small></h5>
+						<p class="card-text">
+							${cardText.value}
+						</p>
+						<button type="button" id="delete-button" class="btn btn-sm btn-light btn-delete">
+							Delete entry
+						</button>
+          </div>
+    `
+    return myHtml;
+  }
+  submitButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    divCard.insertAdjacentHTML("beforebegin", myPost());
 });
